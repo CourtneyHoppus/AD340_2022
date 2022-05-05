1ae2f157cd99c2f4bafb8c9536e39db51424e0c9
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2022-05-05 15:34:53 -0700

    changed MovieViewModel to include director and description created a movieList function in the MovieListActivity to populate a list fo MovieViewModel objects

--- app/src/main/java/com/hoppus/ad340/MovieViewModel.kt
+++ app/src/main/java/com/hoppus/ad340/MovieViewModel.kt
@@ -1,3 +1,6 @@
 package com.hoppus.ad340
 
-data class MovieViewModel(val title: String, val year: String)
+data class MovieViewModel(val title: String,
+                          val year: String,
+                          val director: String,
+                          val description: String)
